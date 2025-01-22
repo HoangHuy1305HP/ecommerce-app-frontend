235fc3571e79d2810ab7b5cd3a0cb71aa68c39f5
HoangHuy1305HP <huyhoangyii@gmail.com>
2025-01-22 13:24:56 +0700

    updateCode

--- src/App.jsx
+++ src/App.jsx
@@ -81,7 +81,7 @@ function App() {
         <Route
             path="/"
             element={
-                <Navigate to="/home" />
+                <Navigate to="/login" />
             }
               >
         </Route>
